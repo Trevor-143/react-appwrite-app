e86ce26bad0301133f46298546fb4ff9951cc0c0
Trevor_143 <trevorarapu2@gmail.com>
2023-08-31 09:59:19 -0400

    intergrated with appwrite

--- src/appwriteConfig/config.js
+++ src/appwriteConfig/config.js
@@ -1,9 +1,15 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '64eeff6c2605c79f89f3'
+export const DATABASE_ID = '64ef076d3d4140f9b1c3'
+export const COLLECTION_ID_MESSAGES = '64ef078ce5140f6859c2'
 
 const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('64eeff6c2605c79f89f3');
+
+export const databases = new Databases(client);
 
 export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
+import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig/config';
+import { ID } from 'appwrite';
 
 function Room() {
+  const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState('')
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let payload = {
+      body:messageBody
+    }
+    let res = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    )
+    setMessageBody('')
+  }
+  
+  const getMessages = async () => {
+    try {
+      const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES);
+      setMessages(response.documents);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  }
+
   return (
-    <>
-      <h1>Room</h1>
-    </>
-  )
+    <main className='container' >
+      <div className='room--container' >
+
+        <form onSubmit={handleSubmit} id="message--form" >
+          <div>
+            <textarea
+              required
+              maxLength="1000"
+              placeholder='Say something...'
+              onChange={(e) => {setMessageBody(e.target.value)}}
+              value={messageBody}
+            >
+            </textarea>
+          </div>
+          <div className='send-btn--wrapper' >
+            <input className='btn btn--secondary' type="submit" value="send" />
+          </div>
+        </form>
+
+        <div>
+          {messages.map(message => (
+            <div key={message.$id} className='message--wrapper' >
+              <div className='message--header' >
+                <p> <small className='message-timestamp' >{message.$createdAt}</small></p>
+              </div>
+              <div className='message--body' >
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      
+    </main>
+  );
 }
 
-export default Room
+export default Room;
