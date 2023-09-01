a56c837ac42df4b376e238903a180149a3d46b29
Trevor_143 <trevorarapu2@gmail.com>
2023-09-01 10:32:14 -0400

    stopped on subscribing

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,7 +1,8 @@
 
 import React, { useEffect, useState } from 'react';
-import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig/config';
-import { ID } from 'appwrite';
+import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig/config';
+import { ID, Query } from 'appwrite';
+import { Trash2 } from 'react-feather';
 
 function Room() {
   const [messages, setMessages] = useState([]);
@@ -9,6 +10,22 @@ function Room() {
 
   useEffect(() => {
     getMessages();
+
+    const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGES}.documents`, response => {
+      if(response.events.includes('databases.*.collections.*.documents.*.create')) {
+        console.log('message was created')
+        setMessages(prevState => [response.payload, ...prevState])
+      }
+      if(response.events.includes('databases.*.collections.*.documents.*.delete')) {
+        console.log('message was deleted')
+        setMessages(prevState => messages.filter(message => message.$id != response.payload.$id ))
+      }
+    });
+
+    return () => {
+      unsubscribe()
+    }
+
   }, []);
 
   const handleSubmit = async (e) => {
@@ -27,11 +44,23 @@ function Room() {
   
   const getMessages = async () => {
     try {
-      const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES);
+      const response = await databases.listDocuments(
+        DATABASE_ID, 
+        COLLECTION_ID_MESSAGES,
+        [ Query.orderDesc('$createdAt'), Query.limit(20) ]
+      );
       setMessages(response.documents);
     } catch (error) {
       console.error("Error fetching messages:", error);
     }
+  }
+
+  const deleteDocument = async (message_id) => {
+    databases.deleteDocument(
+      DATABASE_ID, 
+      COLLECTION_ID_MESSAGES, 
+      message_id 
+    );
   }
 
   return (
@@ -58,7 +87,8 @@ function Room() {
           {messages.map(message => (
             <div key={message.$id} className='message--wrapper' >
               <div className='message--header' >
-                <p> <small className='message-timestamp' >{message.$createdAt}</small></p>
+                <small className='message-timestamp' > { new Date(message.$createdAt).toLocaleString()}</small>
+                <Trash2 className='delete--btn' onClick={() => {deleteDocument(message.$id)}} />
               </div>
               <div className='message--body' >
                 <span>{message.body}</span>
